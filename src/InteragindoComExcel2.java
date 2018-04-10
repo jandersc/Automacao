1d98a1196b42eaea25cc3b5b1f7eebd6346cb1c2
jande <jande@JANDER-NOTE>
2018-04-09 23:58:39 -0300

    Excel

--- src/InteragindoComExcel2.java
+++ src/InteragindoComExcel2.java
@@ -1,28 +1,62 @@
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class InteragindoComExcel2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try{
-		File exc=new File("C:\\TesteSelenium\\Teste.xlsx");
-		FileInputStream bop=new FileInputStream(exc);
-		HSSFWorkbook wb = new  HSSFWorkbook(bop);
-		HSSFSheet ce1 = wb.getSheetAt(0);
 		
-		System.out.println(ce1.getRow(0).getCell(0).getStringCellValue());
-		System.out.println(ce1.getRow(0).getCell(1).getStringCellValue());
-		System.out.println(ce1.getRow(1).getCell(0).getStringCellValue());
-		System.out.println(ce1.getRow(1).getCell(1).getStringCellValue());
-				
+		try{
+			File exc = new File("C:\\TesteSelenium\\Teste.xlsx");
+			FileInputStream bop = new FileInputStream(exc);
+			XSSFWorkbook wb = new XSSFWorkbook(bop);
+			XSSFSheet ce1 = wb.getSheetAt(0);
+
+			for (int r = 0; r < 5; r++) {
+
+				for (int i = 0; i < 5; i++) {
+
+					System.out.println(ce1.getRow(r).getCell(i).getStringCellValue());
+				}
+			}
+
 		}catch(Exception a){
 			System.out.println(a.getMessage());
 		}
 	}
+	
+	public List<String> conteudoPlanilha() {
+
+		List<String> lista = new ArrayList<>();
+		try {
+			File exc = new File("C:\\TesteSelenium\\Teste.xls");
+			FileInputStream bop = new FileInputStream(exc);
+			HSSFWorkbook wb = new HSSFWorkbook(bop);
+			HSSFSheet ce1 = wb.getSheetAt(0);
+
+			for (int r = 0; r < 5; r++) {
+
+				for (int i = 0; i < 5; i++) {
+
+					lista.add(ce1.getRow(r).getCell(i).getStringCellValue());
+
+				}
+			}
+
+		} catch (Exception a) {
+			System.out.println(a.getMessage());
+		}
+
+		return lista;
+	}
+
 
 }
